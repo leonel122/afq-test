320a1061ed1a1006f85f9232f499c5c3acae1a3f
leonel gomez <inf.leonel@gmail.com>
2017-11-27 17:30:07 -0500

    finalizada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,6 @@ class UserController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
